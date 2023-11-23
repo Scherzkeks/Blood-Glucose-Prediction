af98cf30a826307fbb048144464ebca4197c0132
patrick <patrickvr@live.de>
2023-11-23 17:11:39 +0100

    folder for different models will be created automatically Todo's cleared: experiment switch: separat/general add: basal, bolus, carb activation: sigmoid

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import ast
 import sys
 import tensorflow as tf
 from tensorflow import keras
@@ -15,8 +16,13 @@ from keras.optimizers import Adam
 # -------------------- Coding ------------------------------------------------------------------------------------------
 
 VISU_PLOTS = False  # Visualization of Data ON/OFF
-VISU_CORR = True  # Visualization of Correlation matrices ON/OFF
-RUN_MODEL = False  # Model training and testing ON/OFF
+VISU_CORR = False  # Visualization of Correlation matrices ON/OFF
+RUN_MODEL = True  # Model training and testing ON/OFF
+EVAL_MODEL = True  # Evaluation through loading model
+
+METHOD = True  # True == Separately / False == generalized data
+PATIENT_TBD = 0  # TODO: change for different patients
+folder = 'patient_'+str(PATIENT_TBD)+'_method_'+str(METHOD)
 
 # Load Data
 path = "Ohio Data"
@@ -62,25 +68,6 @@ if VISU_PLOTS:
         plt.title('CBG over Time'+str(i))
     plt.show()
 
-# TODO: Parameter changes?
-k = 100  # Number of past BG values
-PH = 5  # Prediction horizon
-
-
-# Prepare sequences for the RNN model
-def sequences(patient_data, window, horizon):
-    X = []
-    Y = []
-
-    # TODO: change back to simple cutting, perhaps with a boolean to switch between sequencing
-    #  the WHOLE_PATIENT_DATA (with or without exception of one patient) or just of ONE_PATIENT
-    for patient in range(len(patient_data)):
-        for idx in range(len(patient_data[patient]) - window - horizon + 1):
-            if max(patient_data[patient]['missing_cbg'].values[idx:idx + window + horizon]) == 0:
-                X.append(patient_data[patient]['cbg'].values[idx:idx + window])
-                Y.append(patient_data[patient]['cbg'].values[idx + window + horizon - 1])
-    return [np.array(X), np.array(Y)]
-
 
 def prepareData(patient_data, normalizeParam=None):
     if normalizeParam is None:
@@ -91,14 +78,15 @@ def prepareData(patient_data, normalizeParam=None):
     for patient in range(len(patient_data)):
         preparedData.append(patient_data[patient].copy(deep=True))
 
+        # TODO-DONE: scipy.cubic_spline input missing data in cbg (not using cubic spline tho)
         # interpolate missing CBG values not with cubic spline interpolation but pchip
         preparedData[patient]['cbg'] = preparedData[patient]['cbg'].interpolate(method='pchip')
         preparedData[patient]['cbg'] = preparedData[patient]['cbg'].fillna(np.mean(preparedData[patient]['cbg']))
 
-        # replace all other NaN by 0
+        # replace all other NaN by 0 TODO-DONE: fill up the carbs and insulin with 0es
         preparedData[patient] = preparedData[patient].fillna(0)
 
-        # normalization
+        # normalization TODO-DONE: Data normalizing between 0 & 1 ?
         for param in normalizeParam:
             if not max(preparedData[patient][param]) == 0:  # avoid div by 0
                 preparedData[patient][param] = preparedData[patient][param]/max(preparedData[patient][param])
@@ -125,8 +113,9 @@ def prepareData(patient_data, normalizeParam=None):
     return preparedData
 
 
-def correlationMatrix(patient_data,prediction_horizon,target='cbg'):
+def correlationMatrix(patient_data, prediction_horizon, target='cbg'):
 
+    # TODO-DONE: correlation matrix which features are important
     corr_matrix_cbg_next = []
 
     for patient in range(len(patient_data)):
@@ -139,7 +128,7 @@ def correlationMatrix(patient_data,prediction_horizon,target='cbg'):
         corr_matrix = patient_data[patient].corr()  # all correlation matrix
 
         # Display the correlation values
-        # TODO : why is hr NaN?
+        # TODO: why is hr NaN?
         if VISU_CORR:
             print(f"Correlation with {target}_next:")
             print(corr_matrix)
@@ -153,76 +142,103 @@ def correlationMatrix(patient_data,prediction_horizon,target='cbg'):
     return mean_corr_matrix_cbg_next
 
 
-# TODO-DONE: Data normalizing between 0 & 1 ?
-# TODO-"DONE(kein cubic spline)": scipy.cubic_spline input missing data in cbg
-# TODO-DONE: fill up the carbs and insulin with 0es => add into input (correlation matrix which features are important)
+# TODO: Parameter changes?
+k = 100  # Number of past BG values
+PH = 5  # Prediction horizon
+
+
+# Prepare sequences for the RNN model
+def sequences(patient_data, window, horizon, X, Y):  # TODO-DONE: add insulin (bolus and basal) and carbs
+
+    # TODO-DONE: change back to simple cutting, perhaps with a boolean to switch between sequencing
+    #  the WHOLE_PATIENT_DATA (with or without exception of one patient) or just of ONE_PATIENT
+    for idx in range(len(patient_data) - window - horizon + 1):
+        # if max(patient_data['missing_cbg'].values[idx:idx + window + horizon]) == 0: # TODO-DONE: because of spline
+        cbg = patient_data['cbg'].values[idx:idx + window]
+        basal = patient_data['basal'].values[idx:idx + window]
+        carb = patient_data['carbInput'].values[idx:idx + window]
+        bolus = patient_data['bolus'].values[idx:idx + window]
+        X.append([cbg, basal, carb, bolus])
+        Y.append(patient_data['cbg'].values[idx + window + horizon - 1])
+    return [np.array(X), np.array(Y)]
+
 
 train_data = prepareData(train_data)
 test_data = prepareData(test_data)
 
 correlationMatrix(train_data, PH)
 
-# TODO: 2 experiments:
+# TODO-DONE: 2 experiments:
 #  patient separately => 12 models
 #  all together generalized (without test patient) => 12 models
 
-X_train, Y_train = sequences(train_data, k, PH)
-X_test, Y_test = sequences(test_data, k, PH)
+X = []
+Y = []
+if METHOD:  # separately
+    X_train, Y_train = sequences(train_data[PATIENT_TBD], k, PH, X, Y)
+    X_test, Y_test = sequences(test_data[PATIENT_TBD], k, PH, X, Y)
+else:  # generalized
+    for patient in range(len(train_data)):
+        if patient == PATIENT_TBD:
+            X_test, Y_test = sequences(test_data[patient], k, PH, X, Y)
+        else:
+            X_train, Y_train = sequences(train_data[patient], k, PH, X, Y)
 
 # Reshaping
-X_train = X_train.reshape(-1, k, 1)
-X_test = X_test.reshape(-1, k, 1)
+X_train = X_train.reshape(-1, k, 4)
+X_test = X_test.reshape(-1, k, 4)
 Y_train = Y_train.reshape(-1, 1)
 Y_test = Y_test.reshape(-1, 1)
 
+
+# TODO-DONE: possible model changes: activation function because of normalized data => SIGMOID?
+# TODO-DONE: (k, 4) if four features cbg, insulin basal, bolus, carb
+# Creating the RNN model
+rnn_model = Sequential()
+rnn_model.add(LSTM(units=64, input_shape=(k, 4), return_sequences=True))
+rnn_model.add(LSTM(units=64))
+rnn_model.add(Dropout(0.2))
+rnn_model.add(Dense(units=1, activation='sigmoid'))
+rnn_model.summary()
+
+# Optimizer
+optimizer = Adam(
+    learning_rate=0.001,
+    beta_1=0.9,
+    beta_2=0.999,
+    )
+
+# compile the model
+rnn_model.compile(
+    optimizer=optimizer,
+    loss="mean_squared_error",
+    metrics=[]
+    )
+
+# saving best model while training
+cp_path = "./trained/"+folder+"/model"
+checkpoint = ModelCheckpoint(
+    cp_path,
+    monitor="val_loss",
+    save_best_only=True,
+    save_weights_only=False,
+    verbose=1,
+    mode="auto"
+    )
+
+
+def scheduler(epoch):
+    """adaptable learning rate"""
+    if epoch < 20:  # Gradually decrease for the first 20 epochs
+        return 1e-2 / (10 * (epoch/2+1))
+    else:
+        return 1e-4
+
+
+lr_scheduler = LearningRateScheduler(scheduler)
+epochs = 20
+
 if RUN_MODEL:
-    # TODO: possible model changes: activation function because of normalized data
-    # TODO: (k, 4) if four features cbg, insulin basal, bolus, carb
-    # Creating the RNN model
-    rnn_model = Sequential()
-    rnn_model.add(LSTM(units=64, input_shape=(k, 1), return_sequences=True))
-    rnn_model.add(LSTM(units=64))
-    rnn_model.add(Dropout(0.2))
-    rnn_model.add(Dense(units=1, activation='linear'))
-    rnn_model.summary()
-
-    # Optimizer
-    optimizer = Adam(
-        learning_rate=0.001,
-        beta_1=0.9,
-        beta_2=0.999,
-        )
-
-    # compile the model
-    rnn_model.compile(
-        optimizer=optimizer,
-        loss="mean_squared_error",
-        metrics=[]
-        )
-
-    # saving best model while training
-    cp_path = "./trained/model"
-    checkpoint = ModelCheckpoint(
-        cp_path,
-        monitor="val_loss",
-        save_best_only=True,
-        save_weights_only=False,
-        verbose=1,
-        mode="auto"
-        )
-
-
-    def scheduler(epoch):
-        """adaptable learning rate"""
-        if epoch < 20:  # Gradually decrease for the first 20 epochs
-            return 1e-2 / (10 * (epoch/2+1))
-        else:
-            return 1e-4
-
-
-    lr_scheduler = LearningRateScheduler(scheduler)
-    epochs = 20
-
     # Train the RNN model
     h = rnn_model.fit(
         X_train,
@@ -233,23 +249,11 @@ if RUN_MODEL:
         callbacks=[checkpoint, lr_scheduler]
         )
 
-    with open('./trained/history.txt', 'w') as f:
+    with open('./trained/'+folder+'/history.txt', 'w') as f:
         f.write(str(h.history))
         f.close()
 
-    # Plot loss across epochs and interpret it
-    plt.figure()
-    plt.plot(h.history['loss'])
-    plt.plot(h.history['val_loss'])
-    plt.title('Model Loss')
-    plt.xlabel('Epoch')
-    plt.xlim(-0.5, epochs-0.5)
-    plt.xticks(range(0, epochs), np.linspace(1, epochs, epochs, dtype=int))
-    plt.ylabel('Loss')
-    plt.legend(['train', 'test'], loc='upper right')
-    # plt.show()
-    plt.savefig('./trained/loss_plot.png')
-
+if EVAL_MODEL:
     # load best model saved
     rnn_model = load_model(
         cp_path,
@@ -257,11 +261,38 @@ if RUN_MODEL:
         compile=True
         )
 
+    with open('./trained/'+folder+'/history.txt', 'r') as f:
+        history = ast.literal_eval(f.read())
+
+    # Plot loss across epochs and interpret it
+    plt.figure()
+    plt.plot(history['loss'])
+    plt.plot(history['val_loss'])
+    plt.title('Model Loss')
+    plt.xlabel('Epoch')
+    plt.xlim(-0.5, epochs - 0.5)
+    plt.xticks(range(0, epochs), np.linspace(1, epochs, epochs, dtype=int))
+    plt.ylabel('Loss')
+    plt.legend(['train', 'test'], loc='upper right')
+    # plt.show()
+    plt.savefig('./trained/'+folder+'/loss_plot.png')
+
+    # Plot learning rate
+    plt.figure()
+    plt.plot(history['lr'])
+    plt.title('Model lr')
+    plt.xlabel('Epoch')
+    plt.xlim(-0.5, epochs - 0.5)
+    plt.xticks(range(0, epochs), np.linspace(1, epochs, epochs, dtype=int))
+    plt.ylabel('Learning rate')
+    # plt.show()
+    plt.savefig('./trained/'+folder+'/lr_plot.png')
+
     # Evaluate the model using MSE and MAE
     mse = rnn_model.evaluate(X_test, Y_test)
 
-    with open('trained/evaluation.txt', 'w') as f:
+    with open('trained/'+folder+'/evaluation.txt', 'w') as f:
         f.write(f'Mean Squared Error (MSE): {mse:.4f}\n')
         f.close()
 
-    # TODO: visualization of predicted values compared to true values to check the validity
+# TODO: visualization of predicted values compared to true values to check the validity
